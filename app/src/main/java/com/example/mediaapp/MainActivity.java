63a990dae254dcc701ad02b190678689ed8accf5
shahriar021 <shahriarc19@gmail.com>
2022-02-01 04:59:00 +0600

    First Commit

---
+++ app/src/main/java/com/example/mediaapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.mediaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private Button play;
+    private Button pause;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
+        seekBar = findViewById(R.id.seekBar);
+        //media player using local source
+        mediaPlayer = MediaPlayer.create(this,R.raw.s);
+        //media player using remote source
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("http://penguinradio.dominican.edu/Sound%20FX%20Collection/Super%20Mario%20Bros.-Going%20down%20a%20pipe.mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                Toast.makeText(MainActivity.this, "Ready to play !!", Toast.LENGTH_SHORT).show();
+                mp.start();
+            }
+        });
+        //mediaPlayer.prepareAsync();
+        mediaPlayer.start();
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
